77939dd4d28fb3f328051f5f72a755c5c67527e4
ThristenJones <Thristenjones@yahoo.com>
2021-11-01 17:33:49 -0500

    Finished all Post CRUD functionality for backend

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require('express');
 const app = express();
 const users = require('./routes/users')
 const auth = require('./routes/auth')
+const post = require('./routes/users');
+
 
 connectDB();
 
 app.use(express.json());
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+app.use('/api/post', post)
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
